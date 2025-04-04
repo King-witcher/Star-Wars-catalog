c65c3d2238d6bf64afaa184a7fdd5b59a071cd22
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-04-04 20:08:38 -0300

    fix: unfinished refactor

---
+++ src/app/script/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+export default function Page() {
+  // const fields: Record<string, string[]> = {}
+  // function addIfNotExists(key: string, value: string) {
+  //   if (fields[key] && !fields[key].includes(value)) fields[key].push(value)
+  //   else if (!fields[key]) fields[key] = [value]
+  // }
+  // for (const person of people) {
+  //   for (const [key, value] of Object.entries(person)) {
+  //     if (typeof value === 'string') {
+  //       addIfNotExists(key, value)
+  //     } else {
+  //       for (const single_value of value) {
+  //         addIfNotExists(key, single_value)
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log(fields)
+  // return <code>{JSON.stringify(people)}</code>
+}
