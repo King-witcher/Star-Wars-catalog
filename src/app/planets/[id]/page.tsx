8673709375904f4ba34e3a9ecda3168df89edd60
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-04-05 15:32:18 -0300

    fix: use promises instead

--- src/app/planets/[id]/page.tsx
+++ src/app/planets/[id]/page.tsx
@@ -6,9 +6,9 @@ import { notFound } from 'next/navigation'
 import { ClientComponent } from './client-component'
 
 interface Props {
-  params: {
+  params: Promise<{
     id: string
-  }
+  }>
 }
 
 export default async function Page({ params }: Props) {
