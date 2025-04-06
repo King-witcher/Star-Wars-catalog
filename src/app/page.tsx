df948a86f133db82736db66b3466f86da2177f98
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-04-06 14:52:57 -0300

    feat: add basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>ola mundo</div>
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Typography from '@mui/material/Typography'
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <Typography variant="h3" color="primary">
+        Explore the Star Wars Catalog!
+      </Typography>
+      <Typography variant="body2" color="primary">
+        By Giuseppe Lanna
+      </Typography>
+      <ButtonGroup size="large" variant="contained" className="mt-[40px]">
+        <Button>People</Button>
+        <Button>Planets</Button>
+      </ButtonGroup>
+    </div>
+  )
 }
